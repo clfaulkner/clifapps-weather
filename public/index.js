8462a50805e01457d01cb23b1efcdc23ac2fd313
Chris Faulkner <mrchrisf.clf@gmail.com>
2020-12-20 19:37:20 -0600

    Worked on JS file and got 3-day data to show in cosole  :-))

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 // SECTION todo's
 // TODO delete unnecessary commented lines
+// TODO dev loop to display 3-day data (consider for...of structure)
 
 
 // * weather vars
@@ -13,26 +14,23 @@ let temp = "Temperature = ",
   st = "mn";
 
 // detail weather vars
-let localCity = "",
-  LocalSt="",
-  LocalCntry="",
-  forecastDate="",
-  forecastDayMaxTemp="",
-  forecastDayMinTemp="",
-  forecastDayMaxWind="",
-  forecastDayHumid="",
-  forecastDayRainChance="",
-  forecastDaySnowChance="",
+let loCation = "Location: ", //location will be city,state combined
+  LocalCntry="Country: ",
+  forecastDate="Date: ",
+  forecastDayMaxTemp="Max Temp: ",
+  forecastDayMinTemp="Min Temp: ",
+  forecastDayMaxWind="Wind: ",
+  forecastDayHumid="Humidity: ",
+  forecastDayRainChance="Rain Chance: ",
+  forecastDaySnowChance="Snow Chance: ",
   forecastDayCndtnTxt="",
   forecastDayCndtnIcon="";
 
 
 
 // SECTION: Set initial city, st or get current from store
-// function store(city,st){
 function store(){
   if(localStorage.getItem('city')===null){
-    // todo: test what heppens when st=values & city=null
       localStorage.setItem('city', city);
       localStorage.setItem('st', st);
     }
@@ -46,7 +44,7 @@ function store(){
 // CALL store()
 store();
 
-// SECTION: fetch weather and details
+// SECTION: fetch current weather
 function getWthr(){
   // declare vars
   let api = "https://api.weatherapi.com/v1/current.json?key="
@@ -64,7 +62,7 @@ function getWthr(){
       document.getElementById("local").innerHTML = data.location.name;
       document.getElementById("desc").innerHTML = data.current.condition.text;
 // FIXME consider using CDN for icon
-      // document.getElementById("icon").innerHTML = data.location.name;
+      // document.getElementById("icon").innerHTML = data.location.**use-icon-var-name;
       document.getElementById("temp-f").innerHTML = temp + data.current.temp_f + " &#x2109;";
       document.getElementById("feelslike-f").innerHTML = butFeels + data.current.feelslike_f + " &#x2109;";
       document.getElementById("wind-mph").innerHTML = wind + data.current.wind_mph;
@@ -85,40 +83,30 @@ function changeWthr(){
   localStorage.setItem('st',st);
 }
 
-// SECTION Get new weather and details
-// psuedo code
-// fetch data
-// then convert to json
-// then manipulate data
-//    let dayCount = TODAYS DATE + 3
-//    for forecast.forecastday.date < dayCount
-//      populate forecast vars
-//    create template
-//    insert template into DOM
-function getDetails(){
-  // declare vars
-  // TODO consider using vars already in getWthr()
-  let api = "https://api.weatherapi.com/v1/forecast.json?key="
-  let apiKey = "7d94c74d88d147c89f5150101201806";
-  let url = api +
-    apiKey + "&q=" +
-    city + " " + st +
-    "&days=3"
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-  }
-
-// CALL getDetails()
-getDetails();
-
 // SECTION HTML constant
-// const webPage = ``
+const webPage = `
+  <div class="w3-modal-content w3-card-4 w3-round-large" style="max-width:500px; background-image: linear-gradient(black,darkblue,DeepSkyBlue);">
+      <span class="w3-button w3-right w3-red" onclick="document.getElementById('three-day').style.display='none'" >&times;</span>
+      <p class="w3-center details">City, State</p>
+      <div class="w3-cell-row">
+        <div class="w3-container w3-cell w3-cell-middle">
+          <p class="details">Icon, description (left)</p>
+        </div>
+        <div class="w3-container w3-cell">
+          <ul>
+            <li class="details" style="list-style-type: none;">wind</li>
+            <li class="details" style="list-style-type: none;">precip</li>
+            <li class="details" style="list-style-type: none;">pressure</li>
+            <li class="details" style="list-style-type: none;">temp</li>
+          </ul>
+        </div>
+      </div>
+      <p class="details">Day, Icon, Temp</p>
+    </div>
+  `
 
 // FIXME update var value names to match return data
-/* var data guide
+/* var forecast data guide
  * Daily details vars (repeats 3 x):
  * forecastDayCndtnIcon= forecast.forecastday.day.condition.icon
  * forecastDayCndtnTxt= forecast.forecastday.day.condition.text
@@ -133,3 +121,32 @@ getDetails();
  * forecastDayRainChance= forecast.forecastday.daily_chance_of_rain
  * forecastDaySnowChance= forecast.forecastday.daily_chance_of_snow
 */
+
+// SECTION Get 3-day weather and details
+// psuedo code
+// fetch data
+// then convert to json
+// then manipulate data
+//    let dayCount = TODAYS DATE + 3
+//    for forecast.forecastday.date < dayCount
+//      populate forecast vars
+//    create template
+//    insert template into DOM
+function getDetails(){
+  // declare vars
+  // TODO consider using vars already in getWthr()
+  let api = "https://api.weatherapi.com/v1/forecast.json?key="
+  let apiKey = "7d94c74d88d147c89f5150101201806";
+  let url = api + apiKey + "&q=" + city + " " + st + "&days=3"
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      for (let i=0; i<3; i++){
+        console.log(data.forecast.forecastday[i].date + ' ' + data.forecast.forecastday[i].day.maxtemp_f);
+      }
+    })
+  }
+
+// CALL getDetails()
+getDetails();
