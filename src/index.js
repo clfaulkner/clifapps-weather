0a84db21a994fedbe213c01dcaca6e19a1d5b360
Chris Faulkner <clf.faulkner@gmail.com>
2021-02-10 21:19:49 -0600

    last minute changes for 02-10-2021; js file changed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // SECTION todo's
 // TODO delete unnecessary commented lines
 // TODO dev loop to display 3-day data (consider for...of structure)
+// TODO research stacking div's -- traversing upwards in DOM
 
 
 // * weather vars
@@ -27,7 +28,7 @@ let forecastDay = "",
   forecastDayRainChance="Rain Chance: ",
   forecastDaySnowChance="Snow Chance: ",
   forecastDayCndtnTxt="",
-  forecastDayCndtnIcon="https:";
+  // forecastDayCndtnIcon="https:"; // we're using ico var
 
 // SECTION: Set initial city, st or get current from store
 function store(){
@@ -104,33 +105,22 @@ function changeWthr(){
 // `
 
 
-// FIXME update var value names to match return data
 /* var forecast data guide
  * Daily details vars (repeats 3 x):
- * forecastDayCndtnIcon= forecast.forecastday.day.condition.icon
- * forecastDayCndtnTxt= forecast.forecastday.day.condition.text
+ * forecastDayCndtnIcon= forecast.forecastday[index].day.condition.icon
+ * forecastDayCndtnTxt= forecast.forecastday[index].day.condition.text
  * localCity= location.name
  * LocalSt= location.region
  * LocalCntry= location.country
- * forecastDate= forecast.forecastday[x].date
- * forecastDayMaxTemp= forecast.forecastday.maxtemp_f
- * forecastDayMinTemp= forecast.forecastday.mintemp_f
+ * forecastDate= forecast.forecastday[index].date
+ * forecastDayMaxTemp= forecast.forecastday[index].maxtemp_f
+ * forecastDayMinTemp= forecast.forecastday[index].mintemp_f
  * forecastDayMaxWind= forecast.forecastday[index].maxwind_mph
- * forecastDayHumid= forecast.forecastday.avghumidity
- * forecastDayRainChance= forecast.forecastday.daily_chance_of_rain
- * forecastDaySnowChance= forecast.forecastday.daily_chance_of_snow
+ * forecastDayHumid= forecast.forecastday[index].avghumidity
+ * forecastDayRainChance= forecast.forecastday[index].daily_chance_of_rain
+ * forecastDaySnowChance= forecast.forecastday[index].daily_chance_of_snow
 */
 
-// SECTION Get 3-day weather and details
-// psuedo code
-// fetch data
-// then convert to json
-// then manipulate data
-//    let dayCount = TODAYS DATE + 3
-//    for forecast.forecastday.date < dayCount
-//      populate forecast vars
-//    create template
-//    insert template into DOM
 function getDetails(){
   // declare vars
   let api = "https://api.weatherapi.com/v1/forecast.json?key=7d94c74d88d147c89f5150101201806"
@@ -147,37 +137,39 @@ function getDetails(){
         console.log(weekDay);
 // SECTION div for details
         let div1= document.createElement("section");
-          div1.className= "w3-row";
+          div1.className= "w3-center";
           div1.id= "deets";
         document.getElementById("three-day").appendChild(div1);
-        let div2= document.createElement("div");
-          div2.className= "w3-col w3-container m6";
-          div2.id= "details";
-          div2.style= "padding-left: 6rem;";
-        document.getElementById("deets").appendChild(div2);
+        // let div2= document.createElement("div");
+        //   div2.className= "w3-col w3-container m6";
+        //   div2.id= "details";
+        //   div2.style= "padding-left: 6rem;";
+        // document.getElementById("deets").appendChild(div2);
         let wd = document.createElement("p");
           wd.className="w3-center";
           wd.innerText= weekDay;
-        document.getElementById("details").appendChild(wd);
+        document.getElementById("deets").appendChild(wd);
         // document.getElementById("three-day").appendChild(wd);
         let detDesc= document.createElement("p");
           detDesc.innerText= forecastDayCndtnTxt+
           data.forecast.forecastday[index].day.condition.text;
-          document.getElementById("details").appendChild(detDesc);
+          document.getElementById("deets").appendChild(detDesc);
         // let detIco= document.createElement("span");
         let detIcoImg= document.createElement("img");
           detIcoImg.src= ico+data.forecast.forecastday[index].day.condition.icon;
-          document.getElementById("details").appendChild(detIcoImg);
+          document.getElementById("deets").appendChild(detIcoImg);
           // <div class="w3-container w3-cell">
-// SECTION div for 2nd column
-        let div3= document.createElement("div");
-          div3.className= "w3-col w3-container m6";
-          div3.id= "side-2"
-          div3.style= "padding-left: 4rem;";
-        document.getElementById("deets").appendChild(div3);
+// // SECTION div for 2nd column
+//         let div3= document.createElement("div");
+//           div3.className= "w3-col w3-container m6";
+//           div3.id= "side-2"
+//           div3.style= "padding-left: 4rem;";
+//         document.getElementById("deets").appendChild(div3);
         let wind= document.createElement("p");
           wind.innerText= forecastDayMaxWind+Math.ceil(data.forecast.forecastday[index].day.maxwind_mph);
-        document.getElementById("side-2").appendChild(wind);
+        document.getElementById("deets").appendChild(wind);
+        let hr= document.createElement("hr");
+        document.getElementById("deets").appendChild(hr);
       }
     })
   }
